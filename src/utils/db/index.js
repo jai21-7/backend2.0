2063343cdbf8afce019c4fd4f06100ba4124d438
jai tripathi <tripathijai05@gmail.com>
2026-03-28 00:48:49 +0530

    Now we know how to connect mongoDB to backend

--- src/utils/db/index.js
+++ src/utils/db/index.js
@@ -7,7 +7,7 @@ const connectDB = async () => {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
         console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance}`);
     } catch (error) {
-        console.log("MONGODB connection error", error);
+        console.log("MONGODB connection FAILED", error);
         process.exit(1)
     }
 }
