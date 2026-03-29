0aa7b4843fc7ab92de3eeec244ef169f277abd31
jai tripathi <tripathijai05@gmail.com>
2026-03-30 04:02:58 +0530

    Api error file is added

---
+++ src/utils/utils/ApiError.js
@@ -0,0 +1,21 @@
+class ApiError extends Error {
+    constructor(
+        statusCode,
+        message= "Something went wrong",
+        error = [],
+        statck=""
+    ){
+        super(message)
+        this.statusCode = statusCode
+        this.data = null
+        this.mesage = message 
+        this.success = false;
+        this.errors = this.errors
+
+        if (stack) {
+            this.stack = stack
+        } else{
+            Error.captureStackTrace(this, this.constructor)
+        }
+    }
+}
